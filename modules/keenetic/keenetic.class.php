61739fab7606558b832b62d8e4f8224d0507383d
Andrey Isupov <eraser1981@gmail.com>
2021-09-14 08:14:22 +0300

    cast devices to array
    fix error " Invalid argument supplied for foreach()"

--- modules/keenetic/keenetic.class.php
+++ modules/keenetic/keenetic.class.php
@@ -413,14 +413,15 @@ function usual(&$out) {
 
 			//Предобработка списка устройств
 			$devices = $getdata['show']['ip']['hotspot']['host'];
+			$devices = is_array($devices) ? $devices : array($devices);
 			foreach ($devices as $valuedev){
 				if($valuedev['name'] == "") $valuedev['name'] = $valuedev['hostname'];
 				if(!isset($valuedev['link'])) $valuedev['link'] = 0;
 				else if($valuedev['link'] == "up") $valuedev['link'] = 1;
 				else if($valuedev['link'] == "down") $valuedev['link'] = 0;
 				if($valuedev['ip'] == "0.0.0.0") $valuedev['link'] = 0;
-				$devmac[$valuedev['mac']] = $valuedev;
-            }
+				$devmac[$valuedev['mac']] = $valuedev
+			}
 			//Проверка изменений
 			$devicesindb = SQLSelect("SELECT ID, ROUTER_ID, TITLE, MAC, IP, STATUS, TYPE_CONNECT, REGISTERED, TRACK, SCRIPT, LINKED_OBJECT, LINKED_PROPERTY, LINKED_METHOD, UPDATED FROM keenetic_devices WHERE ROUTER_ID='".$router['ID']."'");
 			foreach ($devicesindb as $value){ //Если устройство из БД есть в устройствах, отданных роутером
